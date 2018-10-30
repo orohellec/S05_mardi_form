fc38293485d99d130979faa95c636fffdc39f699
Olivier <orohellec44@gmail.com>
2018-10-30 22:49:08 +0100

    no css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,11 +4,11 @@ class UserController < ApplicationController
     @user = User.new
   end
   def create
-  #comment for the third form
+#  comment for the third form
   User.create(username: params[:username], email: params[:email], bio: params[:bio])
-  #uncomment for the third form
+#  uncomment for the third form
 #  @user_params = params[:user]
 #  User.create(username: @user_params[:username], email: @user_params[:email], bio: @user_params[:bio])
-
+  redirect_to '/'
   end
 end
