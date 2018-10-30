bdc7acac871a76bcb95ce73462aad0170b96c1ad
Olivier <orohellec44@gmail.com>
2018-10-30 18:40:47 +0100

    for jerome

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,15 @@
 class UserController < ApplicationController
   def new
     @all_users = User.all
+    @user = User.new
+    puts @user
   end
   def create
-    User.create(username: params[:username], email: params[:email], bio: params[:bio])
+#    User.create(username: params[:username], email: params[:email], bio: params[:bio])
+#    user = User.new
+#    user.email = params[:email]
+#    user.bio = params[:bio]
+# @user = User.new
+  
   end
 end
