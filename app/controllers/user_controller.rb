8d44ab576331dc004e9f18035fc1f62bd37708a1
Olivier <orohellec44@gmail.com>
2018-10-30 19:00:31 +0100

    for the hord

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,14 +2,13 @@ class UserController < ApplicationController
   def new
     @all_users = User.all
     @user = User.new
-    puts @user
   end
   def create
-#    User.create(username: params[:username], email: params[:email], bio: params[:bio])
-#    user = User.new
-#    user.email = params[:email]
-#    user.bio = params[:bio]
-# @user = User.new
-  
+  #comment for the third form
+  User.create(username: params[:username], email: params[:email], bio: params[:bio])
+  #uncomment for the third form
+#  @user_params = params[:user]
+#  User.create(username: @user_params[:username], email: @user_params[:email], bio: @user_params[:bio])
+
   end
 end
